78175a174b63d26d3fb62aa6f6f956abc6ce8c60
Mercy <silvakyle075@gmail.com>
2025-10-15 16:53:43 -0500

    fix some things

--- nosotros/script.js
+++ nosotros/script.js
@@ -1,18 +1,16 @@
-const hero = document.querySelector(".hero-quienes");
+const hero = document.querySelector(".carrusel");
 const images = [
-  "../images/wallpaper.jpg",
-  "../images/wallpaper1.jpeg",
-  "../images/wallpaper2.jpeg",
-  "../images/wallpaper3.jpg",
-  "../images/wallpaper4.jpg",
+  "carrusel/Agencia Nacional de Seguridad Vial - color.png",
+  "carrusel/gob-atl-2024.png",
+  "carrusel/atl2020.png",
 ];
 
 let index = 0;
 
-// Función para cambiar el fondo
+// 🔹 Esta es la función modificada
 function changeBackground(i) {
-  index = (i + images.length) % images.length; // asegura ciclo circular
-  hero.style.backgroundImage = `url(${images[index]})`;
+  index = (i + images.length) % images.length;
+  hero.style.background = `url('${images[index]}') center / cover no-repeat`;
 }
 
 // Botones
